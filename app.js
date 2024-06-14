564b836d8a747584276edae829d293bab2a13727
Oliwia <itsechi@gmail.com>
2024-06-14 20:42:05 +0200

    Test 2

--- app.js
+++ app.js
@@ -110,11 +110,11 @@ app.get(
 );
 
 app.get("/api/user", (req, res) => {
-  if (req.isAuthenticated()) {
-    res.json(req.user);
-  } else {
-    res.status(401).json({ error: "User not authenticated" });
-  }
+  // if (req.isAuthenticated()) {
+  res.json(req.user);
+  // } else {
+  //   res.status(401).json({ error: "User not authenticated" });
+  // }
 });
 
 app.get("/logout", (req, res) => {
